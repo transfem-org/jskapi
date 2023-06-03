45caa065b999f5686ac21861e454efef8de6b8f0
Kainoa Kanter <kainoa@t1c.dev>
2023-06-03 05:38:14 +0000

    Use ethical useragent
    This was carried over from Misskey (https://github.com/joinmisskey/api/blob/23b6a2fad7616cf0387c77d7a5d54456205ed0d4/getInstancesInfos.js#L14). I have no clue why they would spoof a useragent instead of using a legitimate one.

--- getInstancesInfos.js
+++ getInstancesInfos.js
@@ -11,7 +11,7 @@ const instances = loadyaml("./data/instances.yml");
 const pqueue = new Queue(32);
 
 const ua =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0";
+  "CalckeyOrg/0.1.0; +https://calckey.org/join";
 
 function safeFetch(
   method,
