52115ecb7526e46d9e6f26ed0b17a2dedce0fa33
ThatOneCalculator <kainoa@t1c.dev>
2023-06-21 16:34:35 -0700

    fix useragent

--- getInstancesInfos.js
+++ getInstancesInfos.js
@@ -58,7 +58,7 @@ async function fetchJson(method, url, json) {
     headers: {
       "Content-Type": "application/json",
       "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0",
+        "CalckeyOrg/0.1.0; +https://calckey.org/join",
     },
     redirect: "error",
   };
