87549ea14ca7c8927b81f38146f4fdf1be941620
Marie <marie@kaifa.ch>
2023-09-26 20:09:31 +0200

    upd: update UA in another file

--- getInstancesInfos.js
+++ getInstancesInfos.js
@@ -11,7 +11,7 @@ const instances = loadyaml("./data/instances.yml");
 const pqueue = new Queue(128);
 
 const ua =
-  "JoinFirefish/0.1.0; +https://joinfirefish.org/join";
+  "JoinSharkey/0.1.0; +https://joinsharkey.org/join";
 
 function safeFetch(
   method,
@@ -57,7 +57,7 @@ async function fetchJson(method, url, json) {
     headers: {
       "Content-Type": "application/json",
       "User-Agent":
-        "JoinFirefish/0.1.0; +https://joinfirefish.org/join",
+        "JoinSharkey/0.1.0; +https://joinsharkey.org/join",
     },
     redirect: "error",
   };
